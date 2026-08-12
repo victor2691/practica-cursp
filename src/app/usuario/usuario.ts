ab8d7b76eac17323deeb0da3884abeec481f86e3
Victor Salgado <salgadovictor91@gmail.com>
2026-08-11 18:27:46 -0600

    ok

--- src/app/usuario/usuario.ts
+++ src/app/usuario/usuario.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {USUARIOS_FALSOS} from './usuarios-falsos';
+const indicerandom = Math.floor(Math.random() * USUARIOS_FALSOS.length);
 
 @Component({
   selector: 'app-usuario',
@@ -6,4 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './usuario.html',
   styleUrl: './usuario.css',
 })
-export class Usuario {}
+export class Usuario {
+usuarioSeleccionado = USUARIOS_FALSOS[indicerandom];
+
+get rutaimg(){
+return this.usuarioSeleccionado.avatar;
+}
+
+alSeleccionarUsuario(){
+console.log(`Usuario seleccionado: ${this.usuarioSeleccionado.nombre}`);
+}
+
+actualizarUsuario(){
+ //como puedo recargar la pagina en esta funcion
+ const indicerandom = Math.floor(Math.random() * USUARIOS_FALSOS.length);
+ this.usuarioSeleccionado =  USUARIOS_FALSOS[indicerandom];
+}
+
+}
---
+++ src/app/usuario/usuarios-falsos.ts
@@ -0,0 +1,32 @@
+export const USUARIOS_FALSOS = [
+  {
+    id: 'u1',
+    nombre: 'Antonia Céspedes',
+    avatar: 'usuario-1.png',
+  },
+  {
+    id: 'u2',
+    nombre: 'Emilia Torres',
+    avatar: 'usuario-2.png',
+  },
+  {
+    id: 'u3',
+    nombre: 'Marcos Jeremías',
+    avatar: 'usuario-3.png',
+  },
+  {
+    id: 'u4',
+    nombre: 'David Mercado',
+    avatar: 'usuario-4.png',
+  },
+  {
+    id: 'u5',
+    nombre: 'Pamela Chan',
+    avatar: 'usuario-5.png',
+  },
+  {
+    id: 'u6',
+    nombre: 'Adrián Serbio',
+    avatar: 'usuario-6.png',
+  },
+];
